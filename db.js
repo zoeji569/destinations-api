842f3d1aef86646b68ba08b58ce58e88d528f46b
zoeji569 <zoej569@gmail.com>
2021-12-06 15:42:29 -0800

    update

--- db.js
+++ db.js
@@ -1,13 +1,8 @@
 const destinations=[
-    {
-        id:"123456",
-        name: "Space Needle",
-        location: "Seattle",
-    },
-    {
+        {
         id:"456789",
         name:"Hollywood",
-        location:"Los Angeles",
+        location:"Los Angeles"
     }
 ]
 
