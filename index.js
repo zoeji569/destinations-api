f9a38b24a4519235cb6c2acef4d92c66cbda7b6e
zoeji569 <zoej569@gmail.com>
2021-12-06 12:39:36 -0800

    img

--- index.js
+++ index.js
@@ -1,106 +1,96 @@
-const express = require("express");
+const express=require("express");
 const fetch = require("node-fetch");
-const { redirect } = require("statuses");
-let { destinations } = require("./db");
-const { generateUniqueId } = require("./services");
+const server=express();
+let {destinations}=require("./db");
+const {generateUniqueId}=require("./services");
+const cors = require("cors");
+server.use(express.json());  //allows the server to parse body to json
+server.use(cors());
 
-const server = express();
-server.use(express.json());
-// server.use(express.urlencoded());
+let PORT= process.env.PORT || 3000;
+server.listen(PORT,function(){
+    console.log("Server listen on PORT 3000");
+}); // listen to port 3000
 
-server.listen(3000);
+server.get("/destinations",(req,res)=>{
+    res.send(destinations);
+})
 
-// POST => create destinations
-// data => {name^, location^, photo, description}
-server.post("/destinations", async (req, res) => {
-  const { name, location, photo, description } = req.body;
+server.post("/destinations", async (req,res)=>{
+    const {name,location,photo, description} = req.body; //destructure body
 
-  // Make sure we have a name AND location
-  if (
-    name === undefined ||
-    name.length === 0 ||
-    location === undefined ||
-    location.length === 0
-  ) {
-    return res
-      .status(400)
-      .json({ message: "Name and Location are both required" });
-  }
-
-  const dest = { id: generateUniqueId(), name, location };
-
-  const UNSPLASH_URL = `https://api.unsplash.com/photos/random?client_id=bov0CB5rjCu3qiEZpgq9QgMsionOXLhpj6-VNtsjfVs&q=${name} ${location}`;
-
-  const fetchRes = await fetch(UNSPLASH_URL);
-  const data = await fetchRes.json();
-
-  dest.photo = data.urls.small;
-
-  if (description && description.length !== 0) {
-    dest.description = description;
-  }
-
-  destinations.push(dest);
-
-  res.redirect("/destinations");
-});
-
-// GET => read destinations
-// accepts the follow query parameters
-// continent
-server.get("/destinations", (req, res) => {
-  res.send(destinations);
-});
-
-// PUT => edit a destination
-server.put("/destinations/", (req, res) => {
-  const { id, name, location, photo, description } = req.body;
-
-  if (id === undefined) {
-    return res.status(400).json({ message: "id is required" });
-  }
-
-  if (name !== undefined && name.length === 0) {
-    return res.status(400).json({ message: "Name can't be empty" });
-  }
-
-  if (location !== undefined && location.length === 0) {
-    return res.status(400).json({ message: "Location can't be empty" });
-  }
-
-  for (const dest of destinations) {
-    if (dest.id === id) {
-      if (name !== undefined) {
-        dest.name = name;
-      }
-
-      if (location !== undefined) {
-        dest.location = location;
-      }
-
-      if (photo !== undefined) {
-        dest.photo = photo;
-      }
-
-      if (description !== undefined) {
-        dest.description = description;
-      }
-
-      return res.json(dest);
+    if(name===undefined || 
+       name.length===0 ||
+       location === undefined ||
+       location.length ===0
+       )
+    {
+       return res.status(400).json({message: "Name and location are both required"});
     }
-  }
+
+    const dest= {id:generateUniqueId(), name, location};
+    const UNSPLASH_URL = `https://api.unsplash.com/photos/random?client_id=bov0CB5rjCu3qiEZpgq9QgMsionOXLhpj6-VNtsjfVs&query=${name} ${location}`;
+    
+    const fetchRes = await fetch(UNSPLASH_URL);
+    const data = await fetchRes.json();
+    if(photo){
+        dest.photo=photo;
+    }
+    if(description){
+        dest.description=description;
+    }
+    
+
+    destinations.push(dest);
+
+    res.redirect("/destinations");
+    
 });
 
-// DELETE => delete a destination
-// HOW TO GET THE ID from the reqs
-// route parameters /destinations/:id => req.params.id
-// query /destinations?id=198745 => req.query.id
-server.delete("/destinations/:id", (req, res) => {
-  const destId = req.params.id;
+server.delete("/destinations/:id",(req,res)=>{
 
-  const newDestinations = destinations.filter((dest) => dest.id !== destId);
+    const destId = req.params.id;
+    
+    const newDestinations = destinations.filter((dest) => dest.id !== destId);
 
-  destinations = newDestinations;
+    destinations = newDestinations;
+    
+    red.redirect("/destinations");
 
-  res.redirect("/destinations");
 });
+
+
+server.put("/destinations/",(req,res)=>{
+    
+    const {id, name, location, photo, description} = req.body;
+    if(id===undefined){
+        return res.status(400).json({message: "id is required"});
+    }
+    
+    if(name&&name.length===0){
+        return res.status(400).json({message: "Name can't be empty"})
+    }
+    if(location&&location.length===0){
+        return res.status(400).json({message: "Location can't be empty"})
+    }
+    
+    for (const dest of destinations){
+        if(dest.id=id){
+            if(name){
+                dest.name=name;
+            }
+            if(location){
+                dest.location=location;
+            }
+            if(photo){
+                dest.photo=photo;
+            }
+            if(description){
+                dest.description=description;
+            }
+
+            return res.json(dest);
+        }
+    }
+    
+})
