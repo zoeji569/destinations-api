6638fd050a16c06ea796569ff7c78ddb4a91d731
zoeji569 <zoej569@gmail.com>
2021-12-06 10:41:56 -0800

    Back End

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const destinations=[
     {
+        id:"123456",
         name: "Space Needle",
         location: "Seattle"
     },
     {
+        id:"456789",
         name:"Hollywood",
         location:"Los Angeles"
     }
